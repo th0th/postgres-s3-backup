8f2db9da2c60b5e0f8e1216c7c8f7c221f25a890
Gokhan Sari <gokhan@sari.me>
2024-01-15 02:57:58 +0300

    Fix the storage type typo

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -44,7 +44,7 @@ echo "Uploading to S3..."
 rclone copyto \
   --s3-no-check-bucket \
   "./${BACKUP_FILE_NAME}" \
-  ":s3,access_key_id=${AWS_ACCESS_KEY_ID},provider=AWS,region=${AWS_REGION},secret_access_key=${AWS_SECRET_ACCESS_KEY},storage_class=${AWS_S#AWS_S3_STORAGE_CLASS}:${AWS_S3_ENDPOINT}/${BACKUP_FILE_NAME}"
+  ":s3,access_key_id=${AWS_ACCESS_KEY_ID},provider=AWS,region=${AWS_REGION},secret_access_key=${AWS_SECRET_ACCESS_KEY},storage_class=${AWS_S3_STORAGE_CLASS}:${AWS_S3_ENDPOINT}/${BACKUP_FILE_NAME}"
 echo "Uploading to S3... Done."
 
 if [ -n "${WEBGAZER_PULSE_URL}" ]; then
